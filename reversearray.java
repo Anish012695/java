ac54131b4f6a5debf97bc4f82c24657994d9ee7c
Anish012695 <anishmukharjee20578@gmail.com>
2024-03-01 21:58:14 +0530

    01-03-2024

---
+++ reversearray.java
@@ -0,0 +1,22 @@
+public class reversearray {
+    public static void reverse(int arr[]){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[end];
+            arr[end]=arr[start];
+            arr[start]=temp;
+
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,4,6,7,9,10};
+        reverse(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"");
+        }
+        System.out.println();
+    }
+}
